73d98020badf2eae5f037f3ae0ef3d1de57b40cc
Ryan Strauss <ryanrstrauss@icloud.com>
2018-02-28 23:58:04 -0500

    Fixed java structure and added python code

---
+++ python-solutions/areal.py
@@ -0,0 +1,6 @@
+import math
+import sys
+
+if __name__ == '__main__':
+    area = int(sys.stdin.readline().strip())
+    print(math.sqrt(area) * 4)
---
+++ python-solutions/fizzbuzz.py
@@ -0,0 +1,17 @@
+import sys
+
+if __name__ == '__main__':
+    x, y, n = sys.stdin.readline().strip().split(' ')
+    x = int(x)
+    y = int(y)
+    n = int(n)
+
+    for i in range(1, n + 1):
+        if i % x == 0 and i % y == 0:
+            print('FizzBuzz')
+        elif i % x == 0:
+            print('Fizz')
+        elif i % y == 0:
+            print('Buzz')
+        else:
+            print(i)
