32c4bbbed70f3297bf285764fe424f47beefb4bb
Ryan Strauss <ryanrstrauss@icloud.com>
2018-03-10 20:56:32 -0500

    Completed Natrji

--- python-solutions/natrji.py
+++ python-solutions/natrji.py
@@ -5,20 +5,14 @@ if __name__ == '__main__':
     end = sys.stdin.readline().strip().split(":")
     start = list(map(int, start))
     end = list(map(int, end))
-    result = [0, 0, 0]
-    if start[2] <= end[2]:
-        result[2] += end[2] - start[2]
-    else:
-        result[2] += 60 - start[2] + end[2]
-        result[1] -= 1
-    if start[1] <= end[1]:
-        result[1] += end[1] - start[1]
-    else:
-        result[1] += 60 - start[1] + end[1]
-        result[0] -= 1
-    if start[0] <= end[0]:
-        result[0] += end[0] - start[0]
-    else:
-        result[0] += 24 - start[0] + end[0]
+    start_sec = start[0] * 60 * 60 + start[1] * 60 + start[2]
+    end_sec = end[0] * 60 * 60 + end[1] * 60 + end[2]
+    if end_sec <= start_sec:
+        end_sec += 60 * 60 * 24
+    diff = end_sec - start_sec
+    hours = diff // (60 * 60)
+    minutes = (diff - hours * 60 * 60) // 60
+    seconds = diff % 60
+    result = [hours, minutes, seconds]
     result = list(map(lambda x: str(x).zfill(2), result))
     print(":".join(result))
