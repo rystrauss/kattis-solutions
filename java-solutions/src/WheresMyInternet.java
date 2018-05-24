f4c572961bf3330d0fb4d82e6506e68885d88b54
Ryan Strauss <ryanrstrauss@icloud.com>
2018-05-24 16:49:42 -0400

    Fix WheresMyInternet

--- java-solutions/src/WheresMyInternet.java
+++ java-solutions/src/WheresMyInternet.java
@@ -1,20 +1,31 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class WheresMyInternet {
 
     private static int N, M;
-    private static HashMap<Integer, ArrayList<Integer>> connections;
-    private static boolean[] status;
+    private static House[] houses;
     private static int numConnected;
 
-    private static void traverse(int pos) {
-        for (int i : connections.get(pos)) {
-            if (!status[i]) {
-                status[i] = true;
+    private static class House {
+
+        boolean visited;
+        List<House> adjacent;
+
+        House() {
+            visited = false;
+            adjacent = new ArrayList<>();
+        }
+
+    }
+
+    private static void traverse(House pos) {
+        for (House h : pos.adjacent) {
+            if (!h.visited) {
+                h.visited = true;
                 numConnected++;
-                traverse(i);
+                traverse(h);
             }
         }
     }
@@ -24,27 +35,29 @@ public class WheresMyInternet {
             numConnected = 1;
             N = s.nextInt();
             M = s.nextInt();
-            connections = new HashMap<>();
-            status = new boolean[N];
-            status[0] = true;
+            houses = new House[N];
             for (int i = 0; i < N; i++) {
-                connections.put(i, new ArrayList<>());
+                houses[i] = new House();
             }
+            houses[0].visited = true;
             for (int i = 0; i < M; i++) {
                 int a = s.nextInt();
                 int b = s.nextInt();
-                connections.get(a - 1).add(b - 1);
-                connections.get(b - 1).add(a - 1);
+                houses[a - 1].adjacent.add(houses[b - 1]);
+                houses[b - 1].adjacent.add(houses[a - 1]);
             }
-            traverse(0);
+            traverse(houses[0]);
             if (numConnected == N) {
                 System.out.println("Connected");
             } else {
+                StringBuilder out = new StringBuilder();
                 for (int i = 0; i < N; i++) {
-                    if (!status[i]) {
-                        System.out.println(i + 1);
+                    if (!houses[i].visited) {
+                        out.append(i + 1);
+                        out.append('\n');
                     }
                 }
+                System.out.println(out.toString());
             }
         } catch (Exception e) {
             e.printStackTrace();
