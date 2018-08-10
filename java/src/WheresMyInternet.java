964b56c135d41f46337afaa1ca6d1640e2dc84af
Ryan Strauss <ryanrstrauss@icloud.com>
2018-08-09 20:49:06 -0400

    Clean up WheresMyInternet

--- java/src/WheresMyInternet.java
+++ java/src/WheresMyInternet.java
@@ -31,36 +31,33 @@ public class WheresMyInternet {
     }
 
     public static void main(String[] args) {
-        try (Scanner s = new Scanner(System.in)) {
-            numConnected = 1;
-            N = s.nextInt();
-            M = s.nextInt();
-            houses = new House[N];
+        Scanner s = new Scanner(System.in);
+        numConnected = 1;
+        N = s.nextInt();
+        M = s.nextInt();
+        houses = new House[N];
+        for (int i = 0; i < N; i++) {
+            houses[i] = new House();
+        }
+        houses[0].visited = true;
+        for (int i = 0; i < M; i++) {
+            int a = s.nextInt();
+            int b = s.nextInt();
+            houses[a - 1].adjacent.add(houses[b - 1]);
+            houses[b - 1].adjacent.add(houses[a - 1]);
+        }
+        traverse(houses[0]);
+        if (numConnected == N) {
+            System.out.println("Connected");
+        } else {
+            StringBuilder out = new StringBuilder();
             for (int i = 0; i < N; i++) {
-                houses[i] = new House();
-            }
-            houses[0].visited = true;
-            for (int i = 0; i < M; i++) {
-                int a = s.nextInt();
-                int b = s.nextInt();
-                houses[a - 1].adjacent.add(houses[b - 1]);
-                houses[b - 1].adjacent.add(houses[a - 1]);
-            }
-            traverse(houses[0]);
-            if (numConnected == N) {
-                System.out.println("Connected");
-            } else {
-                StringBuilder out = new StringBuilder();
-                for (int i = 0; i < N; i++) {
-                    if (!houses[i].visited) {
-                        out.append(i + 1);
-                        out.append('\n');
-                    }
+                if (!houses[i].visited) {
+                    out.append(i + 1);
+                    out.append('\n');
                 }
-                System.out.println(out.toString());
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(out.toString());
         }
     }
 
