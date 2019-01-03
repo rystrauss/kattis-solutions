23d8d6c1fac6a198d9a165f1b8d07ba1f537e8f0
Ryan Strauss <rystrauss@davidson.edu>
2019-01-03 11:48:43 -0500

    Update SecurityBadge
    Much faster, but still too slow on one of the last test cases

--- java/src/SecurityBadge.java
+++ java/src/SecurityBadge.java
@@ -1,81 +1,80 @@
-/*
-    Note: While this solution passes the first two test cases, it is currently exceeds the time limit on the third.
- */
-
-import java.io.File;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class SecurityBadge {
 
-    private static Range[][] access;
-    private static int rooms, locks, badges;
-    private static int start, end;
-    private static HashSet<Integer> authorizedBadges;
+    private class Lock {
 
-    private static class Range {
+        int low, high;
+        int lowPassed, highPassed;
+        int destination;
 
-        private int min, max;
-
-        private Range(int min, int max) throws IllegalArgumentException {
-            if (min < 1 || max < 1 || max < min)
-                throw new IllegalArgumentException();
-            this.min = min;
-            this.max = max;
-        }
-
-        private Range intersection(Range r) {
-            if (max < r.min || r.max < min)
-                return null;
-            int newMin = Math.max(min, r.min);
-            int newMax = Math.min(max, r.max);
-            return new Range(newMin, newMax);
+        Lock(int low, int high, int destination) {
+            this.low = low;
+            this.high = high;
+            this.destination = destination;
+            lowPassed = Integer.MAX_VALUE;
+            highPassed = Integer.MIN_VALUE;
         }
 
     }
 
-    private static void traverse() {
-        HashSet<Integer> visited = new HashSet<>();
-        traverse(start, new Range(1, badges), visited);
+    private int N, L, B;
+    private List<List<Lock>> rooms;
+    private int S, D;
+    private int permissableBadges;
+
+    private SecurityBadge(int N, int L, int B, int S, int D) {
+        this.N = N;
+        this.L = L;
+        this.B = B;
+        this.permissableBadges = 0;
+        this.rooms = new ArrayList<>(this.N);
+        for (int i = 0; i < this.N; i++)
+            this.rooms.add(new LinkedList<>());
+        this.S = S;
+        this.D = D;
     }
 
-    private static void traverse(int room, Range badges, HashSet<Integer> visited) {
-        if (room == end) {
-            for (int i = badges.min; i <= badges.max; i++)
-                authorizedBadges.add(i);
+    private void buildGraph(FastReader in) throws IOException {
+        for (int i = 0; i < L; i++) {
+            int from = in.nextInt() - 1;
+            int to = in.nextInt() - 1;
+            int low = in.nextInt();
+            int high = in.nextInt();
+            this.rooms.get(from).add(new Lock(low, high, to));
+        }
+    }
+
+    private void traverse(int pos, int low, int high) {
+        if (pos == this.D) {
+            this.permissableBadges += high - low + 1;
             return;
         }
-        if (visited.contains(room))
-            return;
-        visited.add(room);
-        for (int i = 1; i <= rooms; i++) {
-            if (access[room - 1][i - 1] != null) {
-                traverse(i, badges.intersection(access[room - 1][i - 1]), visited);
+        for (Lock lock : rooms.get(pos)) {
+            if ((low <= lock.high && low < lock.lowPassed) || (high >= lock.low && high > lock.highPassed)) {
+                lock.lowPassed = low;
+                lock.highPassed = high;
+                int newLow = Math.max(low, lock.low);
+                int newHigh = Math.min(high, lock.high);
+                traverse(lock.destination, newLow, newHigh);
             }
         }
     }
 
-    public static void main(String[] args) {
-        try (Scanner s = new Scanner(new File("input.txt"))) {
-            rooms = s.nextInt();
-            locks = s.nextInt();
-            badges = s.nextInt();
-            start = s.nextInt();
-            end = s.nextInt();
-            access = new Range[rooms][rooms];
-            for (int i = 0; i < locks; i++) {
-                int from = s.nextInt();
-                int to = s.nextInt();
-                int minBadge = s.nextInt();
-                int maxBadge = s.nextInt();
-                access[from - 1][to - 1] = new Range(minBadge, maxBadge);
-            }
-            authorizedBadges = new HashSet<>();
-            traverse();
-            System.out.println(authorizedBadges.size());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private void solve() {
+        traverse(this.S, 0, this.B);
+    }
+
+    public static void main(String[] args) throws IOException {
+        FastReader in = new FastReader();
+        SecurityBadge securityBadge = new SecurityBadge(in.nextInt(), in.nextInt(), in.nextInt(), in.nextInt() - 1, in.nextInt() - 1);
+        securityBadge.buildGraph(in);
+        in.close();
+        securityBadge.solve();
+        System.out.println(securityBadge.permissableBadges);
     }
 
 }
